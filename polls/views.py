388dc15b1e710d2617251720330c07acd3f31e85
raphaexl <ebatchas@student.1337.ma>
2023-01-21 14:14:56 +0100

    Part4 end

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,27 @@
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 def index(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
